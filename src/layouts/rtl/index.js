86abdcafb843bd3e0e79ebf0d6b1e61e8e206cfd
franklin-raph <igboekwulusifranklin@gmail.com>
2023-05-25 07:07:14 +0100

    reseted the totals state

--- src/layouts/rtl/index.js
+++ src/layouts/rtl/index.js
@@ -53,56 +53,58 @@ function RTL({ brand, routes }) {
   const { size } = typography;
   const { chart, items } = reportsBarChartData;
   const [stake, setStake] = useState(0);
-  const [totals, setTotals] = useState({payout: 0, profit: 0, roi: 0});
+  const [totals, setTotals] = useState({ payout: 0, profit: 0, roi: 0 });
 
-  const [odds, setOdds] = useState([{odd: 0, stake: 0, payout: 0},{odd: 0, stake: 0, payout: 0}]);
-
+  const [odds, setOdds] = useState([
+    { odd: 0, stake: 0, payout: 0 },
+    { odd: 0, stake: 0, payout: 0 },
+  ]);
 
   const handleAddBetDiv = () => {
-    if(odds.length === 3) return;
-    setOdds([...odds, {odd: 0, stake: 0, payout: 0}])
+    if (odds.length === 3) return;
+    setOdds([...odds, { odd: 0, stake: 0, payout: 0 }]);
   };
 
-  function updateOdd(odd, index){
+  function updateOdd(odd, index) {
     const newOdds = odds;
     newOdds[index].odd = odd;
     setOdds(newOdds);
   }
 
   function calculateTotalArbitrage() {
-    let totalOdds = odds.reduce((acc, val) => acc.odd + val.odd);// Sum of inverse odds
+    let totalOdds = odds.reduce((acc, val) => acc.odd + val.odd); // Sum of inverse odds
     totalOdds = totalOdds && totalOdds !== Infinity ? totalOdds : 0;
     const betAmounts = odds.map((odd) => {
-      odd.stake = stake * Math.abs(1 - (odd.odd/ totalOdds));
+      odd.stake = stake * Math.abs(1 - odd.odd / totalOdds);
       odd.payout = odd.odd * odd.stake;
       // console.log({stake, odd, })
-      return odd
+      return odd;
     }); // Calculate bet amounts for each outcome
     setOdds(betAmounts);
 
     const totalPayout = odds.reduce((acc, val) => acc.payout + val.payout);
-    const totalProfit = (totalPayout/2) - stake;
+    const totalProfit = totalPayout / 2 - stake;
 
     setTotals({
       payout: totalPayout,
       profit: totalProfit,
-      roi: (totalProfit/stake) * 100
-    })
+      roi: (totalProfit / stake) * 100,
+    });
   }
 
-  useEffect(()=>{
-    if(!odds || odds.length === 0 && !document) return;
+  useEffect(() => {
+    if (!odds || (odds.length === 0 && !document)) return;
 
-    odds.map((odd, index)=>{
-      document.querySelector("#odd-input-"+index).value = odd <= 0 ? "" : odd;
-    })
-  }, [odds])
+    odds.map((odd, index) => {
+      document.querySelector("#odd-input-" + index).value = odd <= 0 ? "" : odd;
+    });
+  }, [odds]);
 
-  useEffect(()=>{
-    if(!stake && !document) return;
+  useEffect(() => {
+    if (!stake && !document) return;
 
     document.querySelector("#stake-input").value = stake <= 0 ? "" : stake;
-  }, [stake])
+  }, [stake]);
   return (
     <DashboardLayout>
       <DashboardNavbar />
@@ -121,33 +123,41 @@ function RTL({ brand, routes }) {
         </h5>
         <div className="upperArbitrageCalculator">
           <div className="topHeader"></div>
-          {
-            odds.map((odd, index)=>
-              <div key={index}>
-                <div className="singleBet">
-                  <div className="betInput">
-                    <div>
-                      <p>Bet {index+1}</p>
-                      <input type="number" id={"odd-input-"+index} placeholder={`Please Enter Bet ${index+1} Odds`} onChange={(e)=>{
+          {odds.map((odd, index) => (
+            <div key={index}>
+              <div className="singleBet">
+                <div className="betInput">
+                  <div>
+                    <p>Bet {index + 1}</p>
+                    <input
+                      type="number"
+                      id={"odd-input-" + index}
+                      placeholder={`Please Enter Bet ${index + 1} Odds`}
+                      onChange={(e) => {
                         const currentValue = e.target.value;
                         const newOdd = currentValue <= 0 ? "" : currentValue;
-                        updateOdd(Number(newOdd), index)
-                      }}/>
-                    </div>
+                        updateOdd(Number(newOdd), index);
+                      }}
+                    />
                   </div>
-                  <p className="calculatedValue">{odd.stake ? (odd.stake).toFixed(2) : ""}</p>
-                  <p className="calculatedValue">{odd.payout ? (odd.payout).toFixed(2) : ""}</p>
                 </div>
+                <p className="calculatedValue">{odd.stake ? odd.stake.toFixed(2) : ""}</p>
+                <p className="calculatedValue">{odd.payout ? odd.payout.toFixed(2) : ""}</p>
               </div>
-            )
-          }
+            </div>
+          ))}
 
           <div className="betInput">
             <div>
               <p>Stake</p>
-              <input type="text" placeholder="Please Enter Stake" id="stake-input" onChange={e=>{
-                setStake(Number(e.target.value))
-              }}/>
+              <input
+                type="text"
+                placeholder="Please Enter Stake"
+                id="stake-input"
+                onChange={(e) => {
+                  setStake(Number(e.target.value));
+                }}
+              />
             </div>
           </div>
         </div>
@@ -156,21 +166,29 @@ function RTL({ brand, routes }) {
             <button onClick={handleAddBetDiv}>
               <i className="fa-solid fa-circle-plus"></i>MORE BETS
             </button>
-            <button onClick={(e) => {
-              setOdds([{odd: 0, stake: 0, payout: 0}, {odd: 0, stake: 0, payout: 0}]);
-              setStake(0)
-            }}>
+            <button
+              onClick={(e) => {
+                setOdds([
+                  { odd: 0, stake: 0, payout: 0 },
+                  { odd: 0, stake: 0, payout: 0 },
+                ]);
+                setStake(0);
+                setTotals({ payout: 0, profit: 0, roi: 0 });
+              }}
+            >
               <i className="fa-solid fa-rotate"></i>RESET
             </button>
-            <button onClick={()=>{
-              calculateTotalArbitrage()
-            }}>
+            <button
+              onClick={() => {
+                calculateTotalArbitrage();
+              }}
+            >
               <i className="fa-solid fa-calculator"></i>CALCULATE
             </button>
           </div>
           <div className="finalCalculation">
             <div>
-              Total Payout: <span>${(totals.payout/2).toFixed(2)|| "0.00"}</span>
+              Total Payout: <span>${(totals.payout / 2).toFixed(2) || "0.00"}</span>
             </div>
             <div>
               Total Profit: <span>${totals.profit.toFixed(2) || "0.00"}</span>
